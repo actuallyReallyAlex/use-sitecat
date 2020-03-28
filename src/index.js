2e90b027042500dd14f99d38ddd6edc6774fb307
alexlee-dev <alex@alexlee.dev>
2020-03-27 19:35:38 -0700

    ✨ Very basic functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+/**
+ * React Hook for tracking analytics data with SiteCatalyst.
+ * @returns {[]} An array with 2 values. The 1st is siteCatContext - an object to view all SiteCat data. The 2nd is a function to track data objects.
+ */
 const useSiteCat = () => {
-  return "Hello World";
+  // * Check if s object is present
+  if (!window.s) {
+    throw new Error(
+      "No SiteCat object is present on the window object. Please initiate the SiteCat object."
+    );
+  }
+
+  const siteCatContext = window.s;
+
+  /**
+   * Tracks object of values
+   * @param {object} values
+   */
+  const trackEvent = values => {
+    Object.keys(values).forEach(key => {
+      console.log(`TRACKING - Setting ${key} to ${values[key]}`);
+      window.s[key] = values[key];
+    });
+  };
+
+  return [siteCatContext, trackEvent];
 };
 
 export default useSiteCat;
